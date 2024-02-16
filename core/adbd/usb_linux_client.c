11e167bdedebe67da8a50cc7acee849a13449b21
root <root@DESKTOP-JU1IHIS>
2024-02-16 23:58:09 +0800

    add adbd makefile, remove ssl and xdg dependency form below repo https://github.com/99degree/study_record/ originated: https://github.com/chenwr2018/study_record

--- core/adbd/usb_linux_client.c
+++ core/adbd/usb_linux_client.c
@@ -34,8 +34,15 @@
 #define MAX_PACKET_SIZE_FS	64
 #define MAX_PACKET_SIZE_HS	512
 
-#define cpu_to_le16(x)  htole16(x)
-#define cpu_to_le32(x)  htole32(x)
+#if __BYTE_ORDER == __LITTLE_ENDIAN
+# define cpu_to_le16(x) (x)
+# define cpu_to_le32(x) (x)
+#else
+# define cpu_to_le16(x) ((((x) >> 8) & 0xffu) | (((x) & 0xffu) << 8))
+# define cpu_to_le32(x) \
+	((((x) & 0xff000000u) >> 24) | (((x) & 0x00ff0000u) >>  8) | \
+	 (((x) & 0x0000ff00u) <<  8) | (((x) & 0x000000ffu) << 24))
+#endif
 
 struct usb_handle
 {
